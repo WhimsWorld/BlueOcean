027528942382a938f06fff1ceae064420735dc93
Taryn <TarynCovert89@gmail.com>
2023-09-16 16:01:39 -0700

    created initial leaderboard and storycard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Counter from './counter';
+import Home from './features/Home';
 import store from './app/store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Counter />
+      <Home />
     </Provider>
   );
 }
