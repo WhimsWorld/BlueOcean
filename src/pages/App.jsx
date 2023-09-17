5484917de45a4e9db950ec85ba16c16391c8fe47
Taryn <TarynCovert89@gmail.com>
2023-09-16 18:01:55 -0700

    Added basic structure of home components

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import Leaderboard from '../components/Leaderboard';
-import StoryCard from '../components/StoryCard';
+import Leaderboard from '../components/homecomponents/Leaderboard';
+import StoryCard from '../components/homecomponents/StoryCard';
 import StickyNavbar from '../components/StickyNavbar';
+import Filter from '../components/homecomponents/Filter';
+import Search from '../components/homecomponents/Search';
+import Mystories from '../components/homecomponents/Mystories';
+import Categories from '../components/homecomponents/Categories';
 
 export default function App() {
   return (
     <div>
       <StickyNavbar />
+      <Categories />
+      <Mystories />
+      <Filter />
+      <Search />
       <Leaderboard />
       <StoryCard />
     </div>
