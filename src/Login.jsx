ee958ce5d484560967d4a92428fac3f69c828237
Frank Soto <fasfranksoto@gmail.com>
2023-09-16 17:14:43 -0700

    completed firebase sign-up

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+// import { auth, app } from './utils/firebase';
 
 export default function Login() {
+
+  // const navigate = useNavigate();
   return (
     <div>
       <h1>Spooky Seasons</h1>
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom';
 import App from './App';
 import Login from './Login';
+import Signup from './Signup';
 import ErrorPage from './errorPage';
 
 const router = createBrowserRouter([
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/Signup',
+    element: <Signup />,
     errorElement: <ErrorPage />,
   },
 ]);
