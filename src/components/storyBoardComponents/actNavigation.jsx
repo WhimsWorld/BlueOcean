fd088d1d4a387005306bd06331aab4b68330ae95
Jiahou Liu <twtialn@gmail.com>
2023-09-18 10:17:31 -0700

    Init Story Board

---
+++ src/components/storyBoardComponents/actNavigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from '@material-tailwind/react';
+
+export default function ActNavigation() {
+  return (
+    <Card className="mt-6 w-96">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          Act Navigation
+        </Typography>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Button>Read More</Button>
+      </CardFooter>
+    </Card>
+  );
+}
