d682320d26b84fae61328a682ec17348bf2b8352
luckynumberthirteen <jtobono@gmail.com>
2023-09-19 13:05:10 -0700

    Exchanged carousel for menu

--- src/components/storyCreationComponents/StoryCreationForm.jsx
+++ src/components/storyCreationComponents/StoryCreationForm.jsx
@@ -8,9 +8,27 @@ import {
   Textarea,
   Button,
   Typography,
-  Carousel,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
 } from '@material-tailwind/react';
 import { auth } from '../../utils/firebase.js';
+
+function ThemeMenu() {
+  return (
+    <Menu>
+      <MenuHandler>
+        <Button>Select a Theme</Button>
+      </MenuHandler>
+      <MenuList>
+        <MenuItem>Menu Item 1 <img src="https://i.ibb.co/phRKFP3/PC2.png"></img></MenuItem>
+        <MenuItem>Menu Item 2</MenuItem>
+        <MenuItem>Menu Item 3</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
 
 export default function StoryCreationForm() {
   const navigate = useNavigate();
@@ -46,44 +64,12 @@ export default function StoryCreationForm() {
           <Input size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
           <Textarea variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
           <Input type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
+          <ThemeMenu />
         </div>
+        <Button type="submit" className="mt-6 w-1/2 self-center" onClick={handleSubmit}>
+          Create
+        </Button>
       </form>
-      <h3>Select a theme image:</h3>
-      <Carousel
-        className="rounded-xl w-2/3 self-center"
-        navigation={({ setActiveIndex, activeIndex, length }) => (
-          <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-            {new Array(length).fill("").map((_, i) => (
-              <span
-                key={i}
-                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                  activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-                }`}
-                onClick={() => setActiveIndex(i)}
-              />
-            ))}
-          </div>
-        )}
-      >
-        <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="something creative"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="even more creative"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-          alt="fill me in"
-          className="h-full w-full object-cover"
-        />
-      </Carousel>
-      <Button type="submit" className="mt-6 w-1/2 self-center" onClick={handleSubmit}>
-        Create
-      </Button>
     </Card>
   );
 }
