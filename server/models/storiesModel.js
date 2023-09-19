7570d147280ef81799632978a500d9c5facb6adc
Taryn Wiedrick <taryncovert89@gmail.com>
2023-09-19 10:34:34 -0700

    Update storiesModel.js

--- server/models/storiesModel.js
+++ server/models/storiesModel.js
@@ -94,8 +94,19 @@ export const getLeaderboard = async () => {
   const query = 'SELECT * FROM stories INNER JOIN thumbnail_images on thumbnail_images.thumbnail_id = stories.thumbnail_id ORDER BY like_count DESC';
   const result = await executeQuery(query);
   const response = [];
-  for (let i = 0; i < 10; i += 1) {
-    response.push(result.rows[i]);
+export const getLeaderboard = async () => {
+  const query = 'SELECT * FROM stories INNER JOIN thumbnail_images on thumbnail_images.thumbnail_id = stories.thumbnail_id ORDER BY like_count DESC';
+  const result = await executeQuery(query);
+  const response = [];
+  if (result.rows.length < 10) {
+    for (let i = 0; i < result.rows.length; i += 1) {
+      response.push(result.rows[i]);
+    }
+  } else {
+    for (let i = 0; i < 10; i += 1) {
+      response.push(result.rows[i]);
+    }
   }
   return response;
+};
 };
