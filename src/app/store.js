40b521a32275193df49be88c47da792265141b73
Frank Soto <fasfranksoto@gmail.com>
2023-09-19 21:58:28 -0700

    resolved merge conflicts

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import characterReducer from './slices/characterSlice';
+import userReducer from './slices/userSlice';
+import storyReducer from './slices/storySlice';
+import chatReducer from './slices/chatSlice';
 
 const store = configureStore({
   reducer: {
