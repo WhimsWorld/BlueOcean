a438f0000914a83fb9170819ea22b23fe11dae19
luckynumberthirteen <jtobono@gmail.com>
2023-09-19 20:11:06 -0700

    Prevent users from submitting blank fields

--- src/components/storyCreationComponents/StoryCreationForm.jsx
+++ src/components/storyCreationComponents/StoryCreationForm.jsx
@@ -84,18 +84,32 @@ export default function StoryCreationForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     fetchUser();
-    axios.post('/api/stories', {
-      created_by_user_id: 'user1_id',
-      category_id: selectedCategory,
-      narrator_id: 'user1_id',
-      main_image_id: selectedImage.image_id,
-      thumbnail_id: selectedThumbnail.thumbnail_id,
-      title: title,
-      summary: summary,
-      max_characters: maxPlayers,
-    }).then((error) => {
-      return console.log(error);
-    });
+    if (title === '') {
+      document.getElementById("setTitle").focus();
+    } else if (summary === '') {
+      document.getElementById("setSummary").focus();
+    } else if (maxPlayers === '') {
+      document.getElementById("selectPlayers").focus();
+    } else if (selectedImage.image_id === undefined) {
+      document.getElementById("selectImage").focus();
+    } else if (selectedThumbnail.thumbnail_id === undefined) {
+      document.getElementById("selectThumbnail").focus();
+    } else if (selectedCategory === '') {
+      document.getElementById("selectCategory").focus();
+    } else {
+      axios.post('/api/stories', {
+        created_by_user_id: 'user1_id',
+        category_id: selectedCategory,
+        narrator_id: 'user1_id',
+        main_image_id: selectedImage.image_id,
+        thumbnail_id: selectedThumbnail.thumbnail_id,
+        title: title,
+        summary: summary,
+        max_characters: maxPlayers,
+      }).then((error) => {
+        return console.log(error);
+      });
+    }
   };
   return (
     <Card color="transparent" className="w-2/3 mt-1 p-4 flex">
@@ -104,13 +118,13 @@ export default function StoryCreationForm() {
       </Typography>
       <form className="mt-8 mb-2 w-full max-w-screen-lg ">
         <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-          <Textarea variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
-          <Input type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
-          <MenuWithScrollingContent setSelectedImage={setSelectedImage} />
+          <Input id="setTitle" size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <Textarea id="setSummary" variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
+          <Input id="selectPlayers" type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
+          <MenuWithScrollingContent id="selectImage" setSelectedImage={setSelectedImage} />
           {selectedImage.image_url !== '' ? <img alt="" src={selectedImage.image_url} /> : null}
-          <ThumbnailMenu setSelectedThumbnail={setSelectedThumbnail} />
-          <Input type="number" label="Select a Theme" min="1" max="5" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
+          <ThumbnailMenu id="selectThumbnail" setSelectedThumbnail={setSelectedThumbnail} />
+          <Input id="selectCategory" type="number" label="Select a Theme" min="1" max="5" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
         </div>
         <Button type="submit" className="mt-6 w-1/2 self-center" onClick={handleSubmit}>
           Create
