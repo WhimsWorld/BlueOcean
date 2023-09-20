9a28ae52363cd26c29b8f7199ee01610610358b8
Frank Soto <fasfranksoto@gmail.com>
2023-09-20 13:17:13 -0700

    added posting of character, needs to take in story id

--- server/models/characterModel.js
+++ server/models/characterModel.js
@@ -14,21 +14,22 @@ export const getCharactersByUserId = async (userId) => {
 };
 
 export const addCharacter = async (characterData) => {
+  // console.log('charcter data', characterData);
   const {
-    story_id, user_id, image_id, name, strength, weakness, backstory,
+    story_id, user_id, image_id, char_name, strength, weakness, backstory,
   } = characterData;
-
+  // const story_id = 1;
   const query = `
-    INSERT INTO characters (story_id, user_id, image_id, name, strength, weakness, backstory)
+    INSERT INTO characters (story_id, user_id, image_id, char_name, strength, weakness, backstory)
     VALUES ($1, $2, $3, $4, $5, $6, $7)
     RETURNING *`;
-  const values = [story_id, user_id, image_id, name, strength, weakness, backstory];
+  const values = [story_id, user_id, image_id, char_name, strength, weakness, backstory];
   const result = await executeQuery(query, values);
   return result.rows[0];
 };
 
 export const getImages = async () => {
-  const query = 'SELECT url from images';
+  const query = 'SELECT * from images';
   const result = await executeQuery(query);
   return result.rows;
 };
