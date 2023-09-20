53d5b2afbdd43a140637b898a2fae605b599f514
luckynumberthirteen <jtobono@gmail.com>
2023-09-20 16:03:40 -0700

    Focuses on missing fields

--- src/components/storyCreationComponents/StoryCreationForm.jsx
+++ src/components/storyCreationComponents/StoryCreationForm.jsx
@@ -102,8 +102,8 @@ export default function StoryCreationForm() {
         narrator_id: 'user1_id',
         main_image_id: selectedImage.image_id,
         thumbnail_id: selectedThumbnail.thumbnail_id,
-        title: title,
-        summary: summary,
+        title,
+        summary,
         max_characters: maxPlayers,
       }).then((res) => {
         navigate(`/storyBoard/${res.data.story_id}`);
@@ -117,13 +117,13 @@ export default function StoryCreationForm() {
       </Typography>
       <form className="mt-8 mb-2 w-full max-w-screen-lg ">
         <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-          <Textarea variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
-          <Input type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
-          <MenuWithScrollingContent setSelectedImage={setSelectedImage} />
+          <Input id="setTitle" size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <Textarea id="setSummary" variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
+          <Input id="selectPlayers" type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
+          <MenuWithScrollingContent id="selectImage" setSelectedImage={setSelectedImage} />
           {selectedImage.image_url !== '' ? <img alt="" src={selectedImage.image_url} /> : null}
-          <ThumbnailMenu setSelectedThumbnail={setSelectedThumbnail} />
-          <Input type="number" label="Select a Theme" min="1" max="5" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
+          <ThumbnailMenu id="selectThumbnail" setSelectedThumbnail={setSelectedThumbnail} />
+          <Input id="selectCategory" type="number" label="Select a Theme" min="1" max="5" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
         </div>
         <Button type="submit" className="mt-6 w-1/2 self-center" onClick={handleSubmit}>
           Create
@@ -132,14 +132,3 @@ export default function StoryCreationForm() {
     </Card>
   );
 }
-// import { useNavigate } from 'react-router-dom';
-// import { onAuthStateChanged } from 'firebase/auth';
-// import { auth } from '../utils/firebase';
-// import StickyNavbar from '../components/StickyNavbar';
-
-// export default function SimpleRegistrationForm() {
-//   const navigate = useNavigate();
-
-//   const [storyTitle, setStoryTitle] = useState('');
-//   const [description, setDescription] = useState('');
-//   const [participants, setParticipants] = useState(1);
