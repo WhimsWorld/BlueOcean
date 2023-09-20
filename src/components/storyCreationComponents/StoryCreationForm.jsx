ab2730098010f2a88aba3ecc47a25e6eb2cec802
luckynumberthirteen <jtobono@gmail.com>
2023-09-20 11:21:03 -0700

    Repaired story creation

--- src/components/storyCreationComponents/StoryCreationForm.jsx
+++ src/components/storyCreationComponents/StoryCreationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import axios from 'axios';
@@ -14,28 +14,58 @@ import {
   MenuItem,
 } from '@material-tailwind/react';
 import { auth } from '../../utils/firebase.js';
-
-function ThemeMenu() {
+function MenuWithScrollingContent({ setSelectedImage }) {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    axios.get('/api/storyimages')
+      .then((res) => {
+        setImages(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Menu>
       <MenuHandler>
         <Button>Select a Theme</Button>
       </MenuHandler>
-      <MenuList>
-        <MenuItem>Menu Item 1 <img src="https://i.ibb.co/phRKFP3/PC2.png"></img></MenuItem>
-        <MenuItem>Menu Item 2</MenuItem>
-        <MenuItem>Menu Item 3</MenuItem>
+      <MenuList classname="max-h-96">
+        {images.map((image) => {
+          return (<MenuItem key={image.image_id + image.image_url}><img onClick={()=> setSelectedImage({image_url: image.image_url, image_id: image.image_id, category_id: image.category_id})} alt="" src={image.image_url} /></MenuItem>);
+        })}
       </MenuList>
     </Menu>
   );
 }
-
+function ThumbnailMenu({ setSelectedThumbnail }) {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    axios.get('/api/storythumbnails')
+      .then((res) => {
+        setImages(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <Menu>
+      <MenuHandler>
+        <Button>Select a Thumbnail</Button>
+      </MenuHandler>
+      <MenuList>
+        {images.map((image) => {
+          return (<MenuItem key={image.thumbnail_id + image.thumbnail_url}><img onClick={()=> setSelectedThumbnail({thumbnail_url: image.thumbnail_url, thumbnail_id: image.thumbnail_id, category_id: image.category_id})} alt="" src={image.thumbnail_url} /></MenuItem>);
+        })}
+      </MenuList>
+    </Menu>
+  );
+}
 export default function StoryCreationForm() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [summary, setSummary] = useState('');
   const [maxPlayers, setMaxPlayers] = useState('');
-  const [selectedTheme, setSelectedTheme] = useState('');
+  const [selectedImage, setSelectedImage] = useState({});
+  const [selectedThumbnail, setSelectedThumbnail] = useState({});
+  const [selectedCategory, setSelectedCategory] = useState('');
   let uid;
   let displayName;
   async function fetchUser() {
@@ -87,13 +117,13 @@ export default function StoryCreationForm() {
       </Typography>
       <form className="mt-8 mb-2 w-full max-w-screen-lg ">
         <div className="mb-4 flex flex-col gap-6">
-          <Input id="setTitle" size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-          <Textarea id="setSummary" variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
-          <Input id="selectPlayers" type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
-          <MenuWithScrollingContent id="selectImage" setSelectedImage={setSelectedImage} />
+          <Input size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <Textarea variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
+          <Input type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
+          <MenuWithScrollingContent setSelectedImage={setSelectedImage} />
           {selectedImage.image_url !== '' ? <img alt="" src={selectedImage.image_url} /> : null}
-          <ThumbnailMenu id="selectThumbnail" setSelectedThumbnail={setSelectedThumbnail} />
-          <Input id="selectCategory" type="number" label="Select a Theme" min="1" max="5" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
+          <ThumbnailMenu setSelectedThumbnail={setSelectedThumbnail} />
+          <Input type="number" label="Select a Theme" min="1" max="5" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
         </div>
         <Button type="submit" className="mt-6 w-1/2 self-center" onClick={handleSubmit}>
           Create
