cc377f0ee976232bf1a1ccc0afea4377dd2f1947
Jiahou Liu <twtialn@gmail.com>
2023-09-19 19:53:13 -0700

    Update: LiveChat, StoryBoard route

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import characterReducer from './slices/characterSlice';
 import storyReducer from './slices/storySlice';
+import chatReducer from './slices/chatSlice';
 
 const store = configureStore({
   reducer: {
     characters: characterReducer,
     story: storyReducer,
+    chat: chatReducer,
   },
 });
 
--- src/components/storyCreationComponents/StoryCreationForm.jsx
+++ src/components/storyCreationComponents/StoryCreationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import axios from 'axios';
@@ -14,46 +14,17 @@ import {
   MenuItem,
 } from '@material-tailwind/react';
 import { auth } from '../../utils/firebase.js';
-function MenuWithScrollingContent({ setSelectedImage }) {
-  const [images, setImages] = useState([]);
-  useEffect(() => {
-    axios.get('/api/storyimages')
-      .then((res) => {
-        setImages(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+
+function ThemeMenu() {
   return (
     <Menu>
       <MenuHandler>
         <Button>Select a Theme</Button>
       </MenuHandler>
-      <MenuList classname="max-h-96">
-        {images.map((image) => {
-          return (<MenuItem key={image.image_id + image.image_url}><img onClick={()=> setSelectedImage({image_url: image.image_url, image_id: image.image_id, category_id: image.category_id})} alt="" src={image.image_url} /></MenuItem>);
-        })}
-      </MenuList>
-    </Menu>
-  );
-}
-function ThumbnailMenu({ setSelectedThumbnail }) {
-  const [images, setImages] = useState([]);
-  useEffect(() => {
-    axios.get('/api/storythumbnails')
-      .then((res) => {
-        setImages(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-  return (
-    <Menu>
-      <MenuHandler>
-        <Button>Select a Thumbnail</Button>
-      </MenuHandler>
       <MenuList>
-        {images.map((image) => {
-          return (<MenuItem key={image.thumbnail_id + image.thumbnail_url}><img onClick={()=> setSelectedThumbnail({thumbnail_url: image.thumbnail_url, thumbnail_id: image.thumbnail_id, category_id: image.category_id})} alt="" src={image.thumbnail_url} /></MenuItem>);
-        })}
+        <MenuItem>Menu Item 1 <img src="https://i.ibb.co/phRKFP3/PC2.png"></img></MenuItem>
+        <MenuItem>Menu Item 2</MenuItem>
+        <MenuItem>Menu Item 3</MenuItem>
       </MenuList>
     </Menu>
   );
@@ -64,9 +35,7 @@ export default function StoryCreationForm() {
   const [title, setTitle] = useState('');
   const [summary, setSummary] = useState('');
   const [maxPlayers, setMaxPlayers] = useState('');
-  const [selectedImage, setSelectedImage] = useState({});
-  const [selectedThumbnail, setSelectedThumbnail] = useState({});
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedTheme, setSelectedTheme] = useState('');
   let uid;
   let displayName;
   async function fetchUser() {
@@ -84,18 +53,6 @@ export default function StoryCreationForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     fetchUser();
-    axios.post('/api/stories', {
-      created_by_user_id: 'user1_id',
-      category_id: selectedCategory,
-      narrator_id: 'user1_id',
-      main_image_id: selectedImage.image_id,
-      thumbnail_id: selectedThumbnail.thumbnail_id,
-      title: title,
-      summary: summary,
-      max_characters: maxPlayers,
-    }).then((error) => {
-      return console.log(error);
-    });
   };
   return (
     <Card color="transparent" className="w-2/3 mt-1 p-4 flex">
@@ -107,10 +64,7 @@ export default function StoryCreationForm() {
           <Input size="lg" label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
           <Textarea variant="outlined" label="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
           <Input type="number" label="Maximum Players" min="1" max="10" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)} />
-          <MenuWithScrollingContent setSelectedImage={setSelectedImage} />
-          {selectedImage.image_url !== '' ? <img alt="" src={selectedImage.image_url} /> : null}
-          <ThumbnailMenu setSelectedThumbnail={setSelectedThumbnail} />
-          <Input type="number" label="Select a Theme" min="1" max="5" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
+          <ThemeMenu />
         </div>
         <Button type="submit" className="mt-6 w-1/2 self-center" onClick={handleSubmit}>
           Create
