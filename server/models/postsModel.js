f3f2d5bc72fbfa52eb3836d367b45f61d167db9b
Kimberly Tom <kimberlyjtom@gmail.com>
2023-09-19 21:41:39 -0700

    update query to get joined table info

--- server/models/postsModel.js
+++ server/models/postsModel.js
@@ -4,9 +4,38 @@ import { executeQuery } from '../db.js';
 // eslint-disable-next-line import/prefer-default-export
 export const getPosts = async (storyId) => {
   const query = `
-    SELECT *
-    FROM posts
-    WHERE story_id = $1
+  SELECT
+    posts.*,
+    images.image_url AS narrator_image_url,
+    sounds.sound_url,
+    gifs.gif_url,
+    characters.char_name,
+    char_images.image_url AS char_image_url
+  FROM
+    posts
+  LEFT JOIN
+    images
+  ON
+    posts.narrator_image_id = images.image_id
+  INNER JOIN
+    sounds
+  ON
+    posts.sound_id = sounds.sound_id
+  INNER JOIN
+    gifs
+  ON
+    posts.gif_id = gifs.gif_id
+  LEFT JOIN
+    characters
+  ON
+    posts.char_id = characters.char_id
+  LEFT JOIN
+    images AS char_images
+  ON
+    characters.image_id = char_images.image_id
+  WHERE
+  posts. story_id = $1;
+
   `;
 
   const values = [storyId];
