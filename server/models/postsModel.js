d7ce9f52d7d65d3c1fce58c58ac64afe0d8ff0d1
Kimberly Tom <kimberlyjtom@gmail.com>
2023-09-21 09:55:59 -0700

    fix query to left joins and order by asc

--- server/models/postsModel.js
+++ server/models/postsModel.js
@@ -5,36 +5,39 @@ import { executeQuery } from '../db.js';
 export const getPosts = async (storyId) => {
   const query = `
   SELECT
-    posts.*,
-    images.image_url AS narrator_image_url,
-    sounds.sound_url,
-    gifs.gif_url,
-    characters.char_name,
-    char_images.image_url AS char_image_url
-  FROM
-    posts
-  LEFT JOIN
-    images
-  ON
-    posts.narrator_image_id = images.image_id
-  INNER JOIN
-    sounds
-  ON
-    posts.sound_id = sounds.sound_id
-  INNER JOIN
-    gifs
-  ON
-    posts.gif_id = gifs.gif_id
-  LEFT JOIN
-    characters
-  ON
-    posts.char_id = characters.char_id
-  LEFT JOIN
-    images AS char_images
-  ON
-    characters.image_id = char_images.image_id
-  WHERE
-  posts. story_id = $1;
+  posts.*,
+  images.image_url AS narrator_image_url,
+  sounds.sound_url,
+  gifs.gif_url,
+  characters.char_name,
+  char_images.image_url AS char_image_url
+FROM
+  posts
+LEFT JOIN
+  images
+ON
+  posts.narrator_image_id = images.image_id
+LEFT JOIN
+  sounds
+ON
+  posts.sound_id = sounds.sound_id
+LEFT JOIN
+  gifs
+ON
+  posts.gif_id = gifs.gif_id
+LEFT JOIN
+  characters
+ON
+  posts.char_id = characters.char_id
+LEFT JOIN
+  images AS char_images
+ON
+  characters.image_id = char_images.image_id
+WHERE
+  posts.story_id = $1
+ORDER BY
+  date_created
+ASC;
 
   `;
 
