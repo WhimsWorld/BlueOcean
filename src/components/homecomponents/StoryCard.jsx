246e45f96df31949d0e63b7ec065f12b4715163a
Taryn <TarynCovert89@gmail.com>
2023-09-22 13:36:21 -0700

    fixed some styling issues

--- src/components/homecomponents/StoryCard.jsx
+++ src/components/homecomponents/StoryCard.jsx
@@ -71,33 +71,41 @@ export default function StoryCard({
         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
       </CardHeader>
       <CardBody className="flex flex-col p-2 self-start" style={{ width: '70%' }}>
-        <div className="flex flex-row items-center justify-between">
-          <div className="flex items-center">
-            <Typography color="blue-gray" className="flex items-center font-normal self-end">
-              <Typography variant="h4" color="blue-gray" className="font-medium font-croissant">
-                {story.title}
-              </Typography>
-            </Typography>
-          </div>
-          <div className="flex items-center">
-            <IconButton
-              onClick={likeClickHandler}
-              size="sm"
-              style={{ color: likedStories[story.story_id] ? '#F9A03F' : 'white' }}
-              variant="text"
-              className="!absolute top-4 right-4 rounded-full"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-6 w-6"
-              >
-                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-              </svg>
-            </IconButton>
-          </div>
-        </div>
+
+      <div className="flex items-center justify-between">
+  <Typography style={{ maxWidth: '100%' }} color="blue-gray" className="flex items-center font-normal self-end">
+    <Typography variant="h4" color="blue-gray" className="font-medium font-croissant" style={{ maxWidth: '14em' }}>
+      {story.title}
+    </Typography>
+  </Typography>
+
+  <div className="flex items-center">
+    <IconButton
+      onClick={likeClickHandler}
+      size="sm"
+      style={{ color: likedStories[story.story_id] ? '#F9A03F' : 'white' }}
+      variant="text"
+      className="rounded-full ml-4"  // Adjust margin to your preference
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="h-6 w-6"
+      >
+        <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+      </svg>
+    </IconButton>
+
+    <div className="text-blue-gray">
+      {story.like_count}
+    </div>
+  </div>
+</div>
+
+
+
+
         <div className="text-blue-gray" style={{ position: 'absolute', top: '20px', left: story.like_count > 10 ? '655px' : '665px' }}>{story.like_count}</div>
         <Typography color="gray">
           {dateFormat(story.date_created)}
