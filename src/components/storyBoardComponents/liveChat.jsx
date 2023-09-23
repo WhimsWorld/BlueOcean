13547a34e51299266837742fef244d6accdc0ab9
Taryn <TarynCovert89@gmail.com>
2023-09-23 13:32:10 -0700

    some final details

--- src/components/storyBoardComponents/liveChat.jsx
+++ src/components/storyBoardComponents/liveChat.jsx
@@ -56,11 +56,24 @@ export default function LiveChat({ storyId }) {
     <Card
       className="h-full rounded-none rounded-r-xl px-2"
       style={{
-        maxWidth: '320px', backgroundImage: `url(${rightPanel})`, backgroundRepeat: 'round', justifySelf: 'end',
+        maxWidth: '320px',
+        backgroundImage: `url(${rightPanel})`,
+        backgroundRepeat: 'round',
+        justifySelf: 'end',
       }}
     >
-      <h1 className='font-croissant' style={{textAlign: 'center', textDecoration: 'underline', fontSize: '20px', marginTop: '18px'}}><b>Live Chat</b></h1>
-      <div className="chat-messages overflow-y-auto h-96 mt-4" ref={chatRef} style={{border: '1px solid rgba(131, 105, 83, 0.2)', borderRadius: '25px', background: 'rgba(255, 255, 255, 0.6)'}}>
+      <h1 className="font-croissant text-center underline text-2xl mt-6 mb-4">
+        <b>Live Chat</b>
+      </h1>
+      <div
+        className="chat-messages overflow-y-auto h-96 mt-4"
+        ref={chatRef}
+        style={{
+          border: '1px solid rgba(131, 105, 83, 0.2)',
+          borderRadius: '25px',
+          background: 'rgba(255, 255, 255, 0.6)',
+        }}
+      >
         {chatMessages.map((msg) => (
           <div key={msg.message_id} className={`${msg.user_id === userId ? 'ml-16' : ''}`}>
             <div className="user text-sm ml-2">
@@ -74,31 +87,37 @@ export default function LiveChat({ storyId }) {
           </div>
         ))}
       </div>
-      <div className="flex w-5/6 flex-row gap-2 my-4 rounded-[99px] border border-gray-900/10 bg-gray-900/5 p-2">
+      <div className="flex  flex-row gap-2 my-4 mx-auto rounded-[99px] border border-gray-900/10 bg-gray-900/5 p-2">
         <Textarea
           value={message}
           onChange={handleMessageChange}
           rows={1}
           resize
           placeholder="Share your thoughts..."
-          className="min-h-full !border-0 focus:border-transparent"
+          className="min-h-full !border-0 focus:border-transparent mx-auto"
           containerProps={{
             className: 'grid h-full',
           }}
           labelProps={{
             className: 'before:content-none after:content-none',
           }}
-          style={{resize: 'none'}}
+          style={{ resize: 'none' }}
         />
-        <div>
-          <IconButton variant="text" className="rounded-full" onClick={handleSendMessage}>
+        <div className="flex items-center justify-center">
+          <IconButton
+            variant="text"
+            className="rounded-full"
+            onClick={handleSendMessage}
+            color="indigo"
+            style={{ padding: '8px', margin: '4px' }} // Add this style
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth={2}
-              className="h-5 w-5 mr-4"
+              className="h-5 w-5"
             >
               <path
                 strokeLinecap="round"
@@ -106,8 +125,6 @@ export default function LiveChat({ storyId }) {
                 d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
               />
             </svg>
-            {' '}
-
           </IconButton>
         </div>
       </div>
