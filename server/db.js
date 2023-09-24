6d0bb882702220f50631d81d39b58e3653f45a32
Kimberly Tom <kimberlyjtom@gmail.com>
2023-09-23 19:41:24 -0700

    remove template/thumbnail tables, update foreign

--- server/db.js
+++ server/db.js
@@ -11,7 +11,7 @@ const pool = new Pool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASS,
-  database: 'whimsiworlddb',
+  database: process.env.DATABASE_NAME,
   port: 5432,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 
